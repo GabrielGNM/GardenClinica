80809382eccb5dc318ad366fea998d80c0babbdd
GabrielGNM <gabriel.gnm@gmail.com>
2023-11-27 22:16:32 -0300

    Site Apresentacao Parceiro

--- js/Header.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-hamburger').addEventListener('click', function() {
-    const sidebar = document.querySelector('.sidebar');
-    if (sidebar.classList.contains('open')) {
-        sidebar.classList.remove('open');
-    } else {
-        sidebar.classList.add('open');
-    }
-});
