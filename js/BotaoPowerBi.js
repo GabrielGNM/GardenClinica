674e72ad82a563f00b8dde78bd8c900484ce129a
GabrielGNM <gabriel.gnm@gmail.com>
2023-12-13 23:23:05 -0300

    update projeto

---
+++ js/BotaoPowerBi.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var NomeDoBotao = document.getElementById("PowerBiOuEntrarButton");
+    var userDataString = sessionStorage.getItem("user");
+    if (userDataString) NomeDoBotao.innerText = "Dashboard";
+
+    // Adicionar um manipulador de eventos de clique ao botão
+    document.getElementById("PowerBiOuEntrarButton").addEventListener("click", function () {
+      // Verificar se o usuário está logado
+      if (userDataString) {
+        // O usuário está logado, redirecionar para PowerBi.html
+        window.location.href = "PowerBi.html";
+      } else {
+        // O usuário não está logado, redirecionar para Login.html
+        window.location.href = "Login.html";
+      }
+    });
+  });
